56fd61c96d7378854a00488d5172ba2cec127c21
Austin Gabel <agabel@gmail.com>
2024-02-25 15:52:07 -0600

    Added initial configuration

---
+++ lua/plugins/treesitter.lua
@@ -0,0 +1,6 @@
+return {
+    "nvim-treesitter/nvim-treesitter",
+    ensure_installed = { "lua", "vim", "vimdoc", "json", "yaml", "javascript", "html", "css", "python", "rust", "sql", "dockerfile", "c_sharp", "go", "c", "cpp" }
+    
+}
+
